7ba26a7a74ed208254516d804d94d85e1fd4cdd2
Louella Caraang <lcaraang@hawaii.edu>
2019-11-17 13:46:06 -1000

    issue-4

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -56,7 +56,7 @@ export default class Signin extends React.Component {
                   iconPosition="left"
                   name="email"
                   type="email"
-                  placeholder="UH E-mail address"
+                  placeholder="E-mail address"
                   onChange={this.handleChange}
                 />
                 <Form.Input

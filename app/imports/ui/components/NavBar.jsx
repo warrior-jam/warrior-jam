c85a9b9401b5926e56559708f41d9b1aa7ce5b91
Joseph Paragas <josephparagas5@gmail.com>
2019-11-19 10:10:00 -1000

    Landing Page

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,7 +11,7 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted>
+      <Menu style={menuStyle} attached="top" borderless inverted color='green'>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>WarriorJam</Header>
         </Menu.Item>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='landing-background'>
+        <Grid columns={4}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column textAlign='center'>
+            <div className='landing-background-profile-icon'>
+              <Icon size='huge' name='address card'/>
+              <Header as='h1' size='large' inverted>CREATE A PROFILE</Header>
+            </div>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+
+          <Grid.Column textAlign='center'>
+            <div className='landing-background-headphone-icon'>
+              <Icon size='huge' name='headphones'/>
+              <Header as='h1' size='large' inverted>BROWSE MUSIC</Header>
+            </div>
+          </Grid.Column>
+
+          <Grid.Column textAlign='center'>
+            <div className='landing-background-upload-icon'>
+              <Icon size='huge' name='upload'/>
+              <Header as='h1' size='large' inverted>UPLOAD CONTENT</Header>
+            </div>
+          </Grid.Column>
+
+          <Grid.Column textAlign='center'>
+            <div className='landing-background-discovered-icon'>
+              <Icon size='huge' name='users'/>
+              <Header as='h1' size='large' inverted>GET DISCOVERED</Header>
+            </div>
           </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }

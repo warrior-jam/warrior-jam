f1cb625a4ac3970859d3785986a10f6aa329fc27
Louella Caraang <lcaraang@hawaii.edu>
2019-11-20 20:04:28 -1000

    landing page cleanup

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,34 +6,34 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='landing-background'>
-        <Grid columns={4}>
+        <Grid container centered stackable columns={4}>
 
           <Grid.Column textAlign='center'>
-            <div className='landing-background-profile-icon'>
+            <div className='landing-background-icon'>
               <Icon size='huge' name='address card'/>
-              <Header as='h1' size='large' inverted>CREATE A PROFILE</Header>
+              <Header as='h1' size='medium' inverted>CREATE A PROFILE</Header>
             </div>
           </Grid.Column>
 
 
           <Grid.Column textAlign='center'>
-            <div className='landing-background-headphone-icon'>
+            <div className='landing-background-icon'>
               <Icon size='huge' name='headphones'/>
-              <Header as='h1' size='large' inverted>BROWSE MUSIC</Header>
+              <Header as='h1' size='medium' inverted>BROWSE MUSICIANS</Header>
             </div>
           </Grid.Column>
 
           <Grid.Column textAlign='center'>
-            <div className='landing-background-upload-icon'>
+            <div className='landing-background-icon'>
               <Icon size='huge' name='upload'/>
-              <Header as='h1' size='large' inverted>UPLOAD CONTENT</Header>
+              <Header as='h1' size='medium' inverted>UPLOAD CONTENT</Header>
             </div>
           </Grid.Column>
 
           <Grid.Column textAlign='center'>
-            <div className='landing-background-discovered-icon'>
+            <div className='landing-background-icon'>
               <Icon size='huge' name='users'/>
-              <Header as='h1' size='large' inverted>GET DISCOVERED</Header>
+              <Header as='h1' size='medium' inverted>BE DISCOVERED</Header>
             </div>
           </Grid.Column>
 

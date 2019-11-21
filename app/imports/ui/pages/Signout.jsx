bf6fd985920c6fb65a003dbe9b0a658384046160
Joseph Paragas <josephparagas5@gmail.com>
2019-11-21 10:31:43 -1000

    Update Signout.jsx

--- app/imports/ui/pages/Signout.jsx
+++ app/imports/ui/pages/Signout.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 /** After the user clicks the "Signout" link in the NavBar, log them out and display this page. */
 export default class Signout extends React.Component {
   render() {
     Meteor.logout();
     return (
-        <div className='signout-background' >
-          <Grid columns={4}>
-            <Grid.Column textAlign='center'>
-              <div className='landing-background-profile-icon'>
-                <Icon size='huge' name='address card'/>
-                <Header as='h1' size='large' inverted>CREATE A PROFILE</Header>
-              </div>
-            </Grid.Column>
-
-
-            <Grid.Column textAlign='center'>
-              <div className='landing-background-headphone-icon'>
-                <Icon size='huge' name='headphones'/>
-                <Header as='h1' size='large' inverted>BROWSE MUSIC</Header>
-              </div>
-            </Grid.Column>
-
-            <Grid.Column textAlign='center'>
-              <div className='landing-background-upload-icon'>
-                <Icon size='huge' name='upload'/>
-                <Header as='h1' size='large' inverted>UPLOAD CONTENT</Header>
-              </div>
-            </Grid.Column>
-
-            <Grid.Column textAlign='center'>
-              <div className='landing-background-discovered-icon'>
-                <Icon size='huge' name='users'/>
-                <Header as='h1' size='large' inverted>GET DISCOVERED</Header>
-              </div>
-            </Grid.Column>
-
-          </Grid>
-        </div>
+        <Header as="h2" textAlign="center">
+          <p>You are signed out.</p>
+        </Header>
     );
   }
 }

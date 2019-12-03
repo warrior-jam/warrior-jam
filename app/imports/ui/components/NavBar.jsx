b28997a4004d98323e14858d9bf3acdab1a76c98
Louella Caraang <lcaraang@hawaii.edu>
2019-12-03 10:36:14 -1000

    Updated Navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -17,7 +17,7 @@ class NavBar extends React.Component {
         </Menu.Item>
         {this.props.currentUser ? (
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/home" key='home'>Home</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Search Musician</Menu.Item>]
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/musicians" key='musicians'>Browse Musicians</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>

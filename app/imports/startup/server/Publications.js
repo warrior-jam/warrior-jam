38ab91a2afc0d47b20db0cd9992d56fcb5d83308
Louella Caraang <lcaraang@hawaii.edu>
2019-12-04 02:52:02 -1000

    created default musicians

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Users } from '../../api/user/User';
+import { Musicians } from '../../api/musician/Musician';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -31,6 +32,14 @@ Meteor.publish('User', function publish() {
 Meteor.publish('UserAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Users.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish('Musician', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Musicians.find({ owner: username });
   }
   return this.ready();
 });

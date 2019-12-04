1b45082890f5a9a78994968ea286ee5dab02d352
Louella Caraang <lcaraang@hawaii.edu>
2019-12-03 17:27:01 -1000

    skills and genres labels

--- app/imports/ui/components/Musician.jsx
+++ app/imports/ui/components/Musician.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Image, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import { _ } from 'meteor/underscore';
 
 /** Renders a single row in the List musician table. See pages/Listmusician.jsx. */
 class Musician extends React.Component {
@@ -9,12 +10,20 @@ class Musician extends React.Component {
     return (
         <Card centered>
           <Card.Content>
+            <Image floated='right' size='mini' src={this.props.musician.picture} />
             <Card.Header>{this.props.musician.firstName} {this.props.musician.lastName}</Card.Header>
-            <Card.Meta>{this.props.musician.genre}</Card.Meta>
             <Card.Description>
-              {this.props.musician.instrument}
+              {this.props.musician.bio}
             </Card.Description>
-          </Card.Content>
+            <Card.Content extra>
+              {_.map(this.props.musician.skills,
+                  (skill, index) => <Label key={index} >{skill}</Label>)}
+            </Card.Content>
+            <Card.Content extra>
+              {_.map(this.props.musician.genres,
+                  (genre, index) => <Label key={index} >{genre}</Label>)}
+            </Card.Content>
+            </Card.Content>
         </Card>
     );
   }

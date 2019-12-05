ed569fec40c254f74135f26a40ee4fa0842d2554
Daphne Tapia <dmtapia@hawaii.edu>
2019-12-05 01:27:30 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Container, List, Divider } from 'semantic-ui-react';
+import { Segment, Container, List } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -23,7 +23,6 @@ class Footer extends React.Component {
                                 Contact Us
                             </List.Item>
                         </List>
-
                     </Container>
                 </Segment>
             </div>

f8a4ad2a93ecfc3e29441e5bd8f59678fa7c4147
Daphne Tapia <dmtapia@hawaii.edu>
2019-12-17 22:19:27 -1000

    Delete Stuff items

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,7 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
-import { Users } from '../../api/user/User';
 import { Musicians } from '../../api/musician/Musician';
 import { Events } from '../../api/event/Event';
 import { OpenMics } from '../../api/openmic/OpenMic';
@@ -19,21 +18,6 @@ Meteor.publish('Stuff', function publish() {
 Meteor.publish('StuffAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.find();
-  }
-  return this.ready();
-});
-
-Meteor.publish('User', function publish() {
-  if (this.userId) {
-    const username = Meteor.users.findOne(this.userId).username;
-    return Users.find({ owner: username });
-  }
-  return this.ready();
-});
-
-Meteor.publish('UserAdmin', function publish() {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Users.find();
   }
   return this.ready();
 });

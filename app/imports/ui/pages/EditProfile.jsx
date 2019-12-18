7a6f9949ec70225b1f086bf339145c9022741019
Daphne Tapia <dmtapia@hawaii.edu>
2019-12-17 17:59:05 -1000

    merge

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -1,65 +1,68 @@
 import React from 'react';
-import { Musicians, MusicianSchema } from '/imports/api/musician/Musician';
-import { Form, Grid, Loader, Header, Segment } from 'semantic-ui-react';
+import { Grid, Loader, Header, Segment, Form } from 'semantic-ui-react';
+import swal from 'sweetalert';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
-import LongTextField from 'uniforms-semantic/LongTextField';
 import SubmitField from 'uniforms-semantic/SubmitField';
-import swal from 'sweetalert';
+import HiddenField from 'uniforms-semantic/HiddenField';
+import ErrorsField from 'uniforms-semantic/ErrorsField';
 import { Meteor } from 'meteor/meteor';
-import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
-import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import 'uniforms-bridge-simple-schema-2';
+import LongTextField from 'uniforms-semantic/LongTextField';
+import { Musicians, MusicianSchema } from '../../api/musician/Musician';
 import MultiSelectField from '../forms/controllers/MultiSelectField';
 
-/** Renders the Page for adding a document. */
+/** Renders the Page for editing a single document. */
 class EditProfile extends React.Component {
 
-  /** On submit, insert the data. */
+  /** On successful submit, insert the data. */
   submit(data) {
-    const { firstName, lastName, bio, picture, projects, skills, genres, events, _id } = data;
-    Musicians.update(_id, { $set: { firstName, lastName, bio, picture, projects, skills, genres, events } },
-        (error) => {
-          if (error) {
-            swal('Error', error.message, 'error');
-          } else {
-            swal('Success', 'Profile updated successfully', 'success');
-          }
-        });
+    const { firstName, lastName, bio, picture, youtube, soundcloud, website, skills, genres, _id } = data;
+    Musicians.update(_id, { $set: { firstName, lastName, bio, picture, youtube, soundcloud, website, skills, genres } },
+        (error) => (error ?
+            swal('Error', error.message, 'error') :
+            swal('Success', 'Item updated successfully', 'success')));
   }
 
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   renderPage() {
     return (
-        <div className='content'>
-          <Grid container centered>
-            <Grid.Column>
-              <Header as="h2" textAlign="center">Your Profile</Header>
-              <AutoForm schema={MusicianSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
-                <Segment>
-                  <Form.Group widths={'equal'}>
-                    <TextField name='firstName' placeholder={'First Name'}/>
-                    <TextField name='lastName' placeholder={'Last Name'}/>
-                  </Form.Group>
-                  <LongTextField name='bio' placeholder='Write a little bit about yourself.'/>
-                  <Form.Group widths={'equal'}>
-                    <TextField name='picture' placeholder={'URL to picture'}/>
-                    <TextField name='projects' optional={true} placeholder={'Link to Youtube or SoundCloud channel'}/>
-                  </Form.Group>
-                  <Form.Group widths={'equal'}>
-                    <MultiSelectField name='skills' placeholder={'Skills'}/>
-                    <MultiSelectField name='genres' placeholder={'Genres'}/>
-                    <MultiSelectField name='events' placeholder={'Events'}/>
-                  </Form.Group>
-                  <SubmitField value='Update'/>
-                </Segment>
-              </AutoForm>
-            </Grid.Column>
-          </Grid>
-        </div>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Edit Profile</Header>
+            <AutoForm schema={MusicianSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
+              <Segment>
+                <Form.Group widths={'equal'}>
+                  <TextField name='firstName' placeholder={'First Name'}/>
+                  <TextField name='lastName' placeholder={'Last Name'}/>
+                </Form.Group>
+                <LongTextField name='bio' placeholder='Write a little bit about yourself.'/>
+                <Form.Group widths={'equal'}>
+                  <TextField name='picture' placeholder={'URL to picture'}/>
+                  <TextField name='website' optional={true} placeholder={'Link to Website'}/>
+                </Form.Group>
+                <Form.Group widths={'equal'}>
+                  <TextField name='youtube' optional={true} placeholder={'Link to Youtube channel'}/>
+                  <TextField name='soundcloud' optional={true} placeholder={'Link to Soundcloud feed'}/>
+                </Form.Group>
+                <Form.Group widths={'equal'}>
+                  <MultiSelectField name='skills' placeholder={'Skills'}/>
+                  <MultiSelectField name='genres' placeholder={'Genres'}/>
+                </Form.Group>
+                <ErrorsField/>
+                <HiddenField name='owner' />
+                <SubmitField value='Submit'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
@@ -75,6 +78,7 @@ EditProfile.propTypes = {
 export default withTracker(({ match }) => {
   // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
   const documentId = match.params._id;
+  // Get access to Stuff documents.
   const subscription = Meteor.subscribe('Musician');
   return {
     doc: Musicians.findOne(documentId),
